add0b4da4c82356065ead51e786f777051dc5a13
Fatma Gül Kayış <fatmagulkayis46@gmail.com>
2025-07-08 11:11:24 +0300

    Add AuthenticationForm

--- account/views.py
+++ account/views.py
@@ -2,29 +2,34 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def user_login(request):
-    if request.user.is_authenticated and "next" in request.GET:  #kullanıcının login olup olmadığını kontrol eder
+ if request.user.is_authenticated and "next" in request.GET:  #kullanıcının login olup olmadığını kontrol eder
        return render(request,"account/login.html", {"error":"yetkiniz yok. "})
-    if request.method == "POST":
-      username = request.POST["username"]
-      password = request.POST["password"]
+ if request.method == "POST":
+      form = AuthenticationForm(request,data=request.POST)
+      if form.is_valid():
+         username = form.cleaned_data.get("username")
+         password = form.cleaned_data.get("password")
 
-      user=authenticate(request,username=username,password=password)
+         user=authenticate(request,username=username,password=password)
 
-      if user is not None:
-         login(request,user)
-         messages.add_message(request,messages.SUCCESS,"Giriş Başarılı")
-         nextUrl=request.GET.get("next",None)
-         if nextUrl is None:
-            return redirect("index")
+         if user is not None:
+            login(request,user)
+            messages.add_message(request,messages.SUCCESS,"Giriş Başarılı")
+            nextUrl=request.GET.get("next",None)
+            if nextUrl is None:
+               return redirect("index")
+            else:
+               return redirect(nextUrl)
          else:
-            return redirect(nextUrl)
+            return render(request,"account/login.html", {"form":form})
       else:
-         messages.add_message(request,messages.ERROR,"username ya da parola yanlış")
-         return render(request,"account/login.html")
-    else:
-      return render(request,"account/login.html")
+         return render(request,"account/login.html", {"form":form})   
+ else:
+      form = AuthenticationForm()
+      return render(request,"account/login.html",{"form":form})
 
 def user_register(request):
     if request.method == "POST":
