79a7674836107e34ac763a93250a5081e19797ef
Fatma Gül Kayış <fatmagulkayis46@gmail.com>
2025-07-08 09:35:09 +0300

    Integrate Django messages framework

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.models import User
+from django.contrib import messages
 # Create your views here.
 def user_login(request):
     if request.user.is_authenticated and "next" in request.GET:  #kullanıcının login olup olmadığını kontrol eder
@@ -13,13 +14,15 @@ def user_login(request):
 
       if user is not None:
          login(request,user)
+         messages.add_message(request,messages.SUCCESS,"Giriş Başarılı")
          nextUrl=request.GET.get("next",None)
          if nextUrl is None:
             return redirect("index")
          else:
             return redirect(nextUrl)
       else:
-         return render(request,"account/login.html", {"error":"username ya da parola yanlış"})
+         messages.add_message(request,messages.ERROR,"username ya da parola yanlış")
+         return render(request,"account/login.html")
     else:
       return render(request,"account/login.html")
 
@@ -62,5 +65,6 @@ def user_register(request):
      return render(request,"account/register.html")
 
 def user_logout(request):
+    messages.add_message(request,messages.SUCCESS,"Çıkış Başarılı")
     logout(request)
     return redirect("index")
